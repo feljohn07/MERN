93a5ef30fdbfae7a664e8b537683df4e7cc500a7
Fel John <feljohn.loe.bangasan@gmail.com>
2023-05-29 13:14:13 +0800

    may rows and columns dynamic

--- src/components/LoadingTable.js
+++ src/components/LoadingTable.js
@@ -1,63 +1,32 @@
 import React from 'react'
 
 export default function LoadingTable() {
-  return (
-    <>
-        <tr  className='placeholder-glow'>
-            <th><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></th>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-        </tr>
-        <tr  className='placeholder-glow'>
-            <th><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></th>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-        </tr>
-        <tr  className='placeholder-glow'>
-            <th><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></th>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-        </tr>
-        <tr  className='placeholder-glow'>
-            <th><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></th>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-        </tr>
-        <tr  className='placeholder-glow'>
-            <th><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></th>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-        </tr>
-        <tr  className='placeholder-glow'>
-            <th><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></th>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-            <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>
-        </tr>
+
+    const row = 4;
+    const col = 3;
+
+    const renderTable = () => {
+        const table = [];
+
+        for (let i = 0; i < row; i++) {
+            const row = [];
+
+            for (let j = 0; j < col; j++) {
+                row.push(                
+                        <td><span className='placeholder d-inline-block text-truncate' style={{minWidth: "100%"}}></span></td>                
+                    );
+            }
+
+            table.push(<tr className='placeholder-glow' key={i}>{row}</tr>);
+        }
+
+        return table;
+    };
+    
+    return (
         
-    </>
-  )
+        <>  
+            {renderTable()}            
+        </>
+    )
 }
